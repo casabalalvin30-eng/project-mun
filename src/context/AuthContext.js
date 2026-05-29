74e01c1674bbf319adf1f7b3b1e53ec803464532
ALVINP19 <panganibanalvin4@gmail.com>
2026-05-30 07:49:11 +0800

    new

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
-const API_URL = 'https://projectmun.great-site.net/backend/api';
+const API_URL = 'https://projectmun.kesug.com/backend/api';
 const AuthContext = createContext(null);
 
 export const useAuth = () => {
